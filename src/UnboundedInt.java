55778f8240079187de3683887b4a6155faf34d3f
Mike <lammara96@gmail.com>
2021-03-01 16:38:24 -0500

    Initial commit

---
+++ src/UnboundedInt.java
@@ -0,0 +1,24 @@
+public class UnboundedInt {
+
+    private int manyNodes;
+    private IntNode head;
+    private IntNode back;
+    private IntNode cursor;
+
+    public UnboundedInt(String s) {
+
+    }
+    public UnboundedInt add(UnboundedInt other) {};
+    public UnboundedInt multiply(UnboundedInt other) {};
+    public void addEnd(int i) {};
+    @Override
+    public UnboundedInt clone() {};
+    public boolean equals(Object o) {};
+    public String toString() {};
+    public void start() {};
+    public void advance() {};
+    public int getNodeValue() {};
+    public String toStringNoCommas() {};
+
+
+}
